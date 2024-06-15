c8125c4d8f8b5ee956c3fd80c9e2ba273ee4e412
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-06-14 21:35:58 -0300

    fix: Post props

--- src/components/Post/Post.css.ts
+++ src/components/Post/Post.css.ts
@@ -1,10 +1,6 @@
 import styled from "styled-components";
 
-export type StyledPostProps = {
-    $active: boolean
-}
-
-export const StyledPost = styled.div<StyledPostProps>`
+export const StyledPost = styled.div`
     background-color: ${({theme}) => theme.color.gray_800};
     border-radius: ${({theme}) => theme.border.radius};
     padding: 2.5rem;
